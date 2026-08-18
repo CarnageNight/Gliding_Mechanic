0341c826a404389543df63800a034c427da85b41
Lamin-Ceesay <109028477+CarnageNight@users.noreply.github.com>
2026-08-18 05:36:41 +0100

    Refactor gliding character controls and add thrust mechanics

--- Source/FairyLocked/GlidingCharacter.cpp
+++ Source/FairyLocked/GlidingCharacter.cpp
@@ -20,23 +20,37 @@ void AGlidingCharacter::ProcessKeyPitch(float Rate)
 void AGlidingCharacter::ProcessKeyRoll(float Rate)
 {
 	if (FMath::Abs(Rate) > .2f)
-		ProcessPitch(Rate * 2.f);
+		ProcessRoll(Rate * 2.f);
 }
 
-void AGlidingCharacter::ProcessMouseYInput(float Rate)
+void AGlidingCharacter::ProcessMouseYInput(float Value)
 {
+	ProcessPitch(Value);
 }
 
-void AGlidingCharacter::ProcessMouseXInput(float Rate)
+void AGlidingCharacter::ProcessMouseXInput(float Value)
 {
+	ProcessRoll(Value);
 }
 
 void AGlidingCharacter::ProcessRoll(float Value)
 {
+	bIntentionalRoll = FMath::Abs(Value) > 0.f;
+	
+	if (bIntentionalPitch && !bIntentionalRoll) return;
+	
+	const float TargetRollSpeed = bIntentionalRoll ? (Value * RollRateMultiplier) : (GetActorRotation().Roll * -2.f);
+	
+	CurrentRollSpeed =  FMath::FInterpTo(CurrentRollSpeed, TargetRollSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
 }
 
 void AGlidingCharacter::ProcessPitch(float Value)
 {
+	bIntentionalPitch = FMath::Abs(Value) > 0.f;
+	
+	const float TargetPitchSpeed = Value * PitchRateMultiplier;
+	
+	CurrentPitchSpeed =  FMath::FInterpTo(CurrentPitchSpeed, TargetPitchSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
 }
 
 // Called when the game starts or when spawned
@@ -49,8 +63,46 @@ void AGlidingCharacter::BeginPlay()
 // Called every frame
 void AGlidingCharacter::Tick(float DeltaTime)
 {
+	
+	//TO DO:: make so only apply thrust when holdding W
+			
+	//Calculate Thrust
+	const float CurrentAcc = -GetActorRotation().Pitch * DeltaTime * Acceleration;
+	const float NewForwardSpeed = CurrentForwardSpeed + CurrentAcc;
+	CurrentForwardSpeed = FMath::Clamp(NewForwardSpeed, MinSpeed, MaxSpeed);
+	
+	const FVector LocalMove = FVector(CurrentForwardSpeed * DeltaTime, 0.f, 0.f);
+	
+	AddActorLocalOffset(LocalMove, true);
+	
+	
+	
+	FRotator DeltaRotation(0, 0, 0);
+	DeltaRotation.Roll = CurrentRollSpeed * DeltaTime;
+	DeltaRotation.Yaw = CurrentYawSpeed * DeltaTime;
+	DeltaRotation.Pitch = CurrentPitchSpeed * DeltaTime;
+	
+	AddActorLocalRotation(DeltaRotation);
+	
+	GEngine -> AddOnScreenDebugMessage(0, 0.f, FColor::Green, FString::Printf(TEXT("CurrentForwardSpeed: %f"), CurrentForwardSpeed));
+	
 	Super::Tick(DeltaTime);
 
+}
+
+void AGlidingCharacter::NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other,
+	class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+	
+	//Deflects off teh surface
+	const FRotator CurrentRotation = GetActorRotation();
+	SetActorRotation(FQuat::Slerp(CurrentRotation.Quaternion(), HitNormal.ToOrientationQuat(), .025f));
+	
+	//Slow down
+	CurrentForwardSpeed = FMath::FInterpTo(CurrentForwardSpeed, MinSpeed, GetWorld()->GetDeltaSeconds(), 5.f);
+	
 }
 
 // Called to bind functionality to input
--- Source/FairyLocked/GlidingCharacter.h
+++ Source/FairyLocked/GlidingCharacter.h
@@ -10,20 +10,36 @@ UCLASS()
 class FAIRYLOCKED_API AGlidingCharacter : public ACharacter
 {
 	GENERATED_BODY()
-	
-	float Acceleration{30.f};
-	float MaxSpeed{4000.f};
-	float MinSpeed{500.f};
-	
-	float CurrentForwardSpeed{500.f};
-	
-	float CurrentYawSpeed;
-	float CurrentPitchSpeed;
-	float CurrentRollSpeed;
 
 public:
 	// Sets default values for this character's properties
 	AGlidingCharacter();
+	
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float Acceleration{30.f};
+	
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float MaxSpeed{4000.f};
+	
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float MinSpeed{500.f};
+	
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float RollRateMultiplier{200.f};
+	
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float PitchRateMultiplier{200.f};
+	
+	UPROPERTY(VisibleAnywhere, Category = "Flight")
+	float CurrentForwardSpeed{500.f};
+	
+	
+	float CurrentYawSpeed;
+	float CurrentPitchSpeed;
+	float CurrentRollSpeed;
+	
+	bool bIntentionalPitch{false};
+	bool bIntentionalRoll{false};
 
 protected:
 	void ProcessKeyPitch(float Rate);
@@ -42,6 +58,8 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
